f020a40faad78ccace3c40d2fc74c8967ab3fed3
fatima <merzoukfatimazahraa@gmail.com>
2023-07-21 02:51:36 +0200

    Assignement Completed

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code in this file!
+const currentUser="Fatima"
+const welcomeMessage=`Welcome to Flatbook, ${currentUser}!`
+//with reading readme
+const excitedWelcomeMessage=welcomeMessage.toUpperCase()
+const shortGreeting=`Welcome, ${currentUser.slice(0,2)}!`
+// with just reading npm test
+/*const excitedWelcomeMessage=`WELCOME TO FLATBOOK, ${currentUser.toUpperCase()}!`
+const shortGreeting=`Welcome, ${currentUser.charAt(0)}!`*/
